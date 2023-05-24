4f31c1bcb8cb3ca3f6aacfcaf8fb55870c0e5030
madu5 <MM218@outlook.com.br>
2023-05-24 17:14:19 -0300

    Copy to clipboard button

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     EditText user_input;
     TextView displayEmoji, codeHTML, codeUnicode;
     JSONObject jsonObject;
+    ClipboardManager clipboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +40,16 @@ public class MainActivity extends AppCompatActivity {
         displayEmoji = (TextView) findViewById(R.id.textView);
         codeHTML = (TextView) findViewById(R.id.html_code);
         codeUnicode = (TextView) findViewById(R.id.unicode_code);
+        clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+
 
         try {
+            Log.i("Debug", "DEBUG: PRIMEIRO TRY ENTROU");
             AssetManager assetManager = getAssets();
             InputStream emojisInfos = assetManager.open("data.json");
+
+            if (emojisInfos != null) {Log.i("Debug", "emojisInfos is not null");}
+            else {Log.i("Debug", "emojisInfos is null");}
 
             int size = emojisInfos.available();
             byte[] buffer = new byte[size];
@@ -40,25 +57,46 @@ public class MainActivity extends AppCompatActivity {
             emojisInfos.close();
 
             String json = new String(buffer, "UTF-8");
+            if (json != null) {
+                Log.i("Debug", "json is not null");
+                Log.i("Debug", json);
+            }
+            else {Log.i("Debug", "json is null");}
+
             jsonObject = new JSONObject(json);
 
+            if (jsonObject != null) {Log.i("Debug", "jsonObject is not null");}
+            else {Log.i("Debug", "jsonObject is null");}
+
+            Log.i("Debug", "DEBUG: PRIMEIRO TRY SAIU");
+
         } catch (Exception e) {
+            Log.i("Debug", "DEBUG: PRIMEIRO CATCH");
             e.printStackTrace();
         }
-
     }
 
     public void displayInfo(View view) {
         String emoji = user_input.getText().toString();
         displayEmoji.setText(emoji);
+        Log.i("Debug", "DEBUG: " + emoji);
 
         try {
+            Log.i("Debug", "DEBUG: SEGUNDO TRY ENTROU");
             JSONObject infos = jsonObject.getJSONObject(emoji);
             codeHTML.setText(infos.getString("html"));
             codeUnicode.setText(infos.getString("unicode"));
+            Log.i("Debug", "DEBUG: SEGUNDO TRY SAIU");
 
         } catch (JSONException e) {
-            e.printStackTrace();
+            Log.i("Debug", "DEBUG: SEGUNDO CATCH");
+            e.printStackTrace(); // Criar um tratamento melhor para quando não tiver o emoji pesquisado
         }
+    }
+
+    public void copyInfo(View view) {
+        ClipData clip = ClipData.newPlainText("label", codeUnicode.getText());
+        clipboard.setPrimaryClip(clip);
+
     }
 }
